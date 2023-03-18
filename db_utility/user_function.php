274de7b0db41e9699696933be1f7f214e64424a1
JerryLivano <90676468+JerryLivano@users.noreply.github.com>
2023-03-18 16:31:38 +0700

    tambah fungsi register

--- db_utility/user_function.php
+++ db_utility/user_function.php
@@ -11,3 +11,23 @@ function login($email, $password)
     $link = null;
     return $user;
 }
+
+function addNewUser($newName, $newEmail, $newPassword)
+{
+    $result = 0;
+    $link = createMySQLConnection();
+    $link -> beginTransaction();
+    $query = 'INSERT INTO user(name, email, password) VALUES (?, ?, MD5(?))';
+    $stmt = $link -> prepare($query);
+    $stmt -> bindParam(1, $newName);
+    $stmt -> bindParam(2, $newEmail);
+    $stmt -> bindParam(3, $newPassword);
+    if ($stmt -> execute()) {
+      $link -> commit();
+      $result = 1;
+    } else {
+      $link -> rollBack();
+    }
+    $link = null;
+    return $result;
+}
